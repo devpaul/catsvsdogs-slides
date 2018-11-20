1c9746cbb414351cfa6988ed9a45d10f37ce848c
devpaul <paul@devpaul.com>
2018-11-20 16:32:00 -0700

    Updated readme. Cleaned up publish.

--- README.md
+++ README.md
@@ -1,26 +1,19 @@
-# Presentation Base
+# Phoenix TypeScript November 2018
+
+Slides for [Phoenix Typescript](https://www.meetup.com/Phoenix-TypeScript/events/255940576/) November 2018 meetup. We look at some of the additions in TS 3.1 and what is coming in 3.2 & 3.3, as well as a look at using Web Animations and Web Audio with TypeScript.
 
 Created by Paul Shannon [@developerPaul](https://twitter.com/developerPaul).
 
 ## Quick Start
 
 * `npm install`
-* `grunt serve`
+* Build Css w/ `npm run build`
+* Start the server w/ `npm run start`
 * open [Presentation @ http://localhost:8765](http://localhost:8765)
 
 ## Usage
 
 * Press `s` to open speaker notes
-
-## Dependencies
-
-* [Node.js & npm](https://nodejs.org/download/)
-* [Grunt](http://gruntjs.com/getting-started)
-* [Bower](http://bower.io/#install-bower)
-
-```
-npm install -g grunt-cli
-```
 
 ## Attribution and License 
 
--- support/scripts/publish.ts
+++ support/scripts/publish.ts
@@ -6,27 +6,35 @@ import {distDirectory} from "./constants";
 const ghpages = require('gh-pages');
 
 async function createDist() {
-    await cpy([
-        'index.html',
-        'favicon.ico',
-        'assets/**/*',
-        'resources/**/*.css',
-        'node_modules/reveal.js/**/*',
-        'src/**/*'
-    ], distDirectory, {
-        parents: true
-    });
-    closeSync(openSync(join(distDirectory, '.nojekyll'), 'w'));
+	await cpy([
+		'index.html',
+		'favicon.ico',
+		'assets/**/*',
+		'resources/**/*.css',
+		'node_modules/reveal.js/**/*',
+		'src/**/*'
+	], distDirectory, {
+		parents: true
+	});
+	closeSync(openSync(join(distDirectory, '.nojekyll'), 'w'));
 }
 
 async function publish() {
-    await new Promise((resolve, reject) => {
-        ghpages.publish(distDirectory, {
-            dotfiles: true,
-            push: true
-        }, () => resolve())
-    });
-    console.log('Published!');
+	await new Promise((resolve, reject) => {
+		ghpages.publish(distDirectory, {
+			dotfiles: true,
+			push: true
+		}, (err) => {
+			if (err) {
+				console.log('failed', err);
+				reject(err);
+			}
+			else {
+				console.log('published');
+				resolve();
+			}
+		})
+	});
 }
 
 createDist();
